12f8c4a61b1c66e0acd27553a8829989756bffc7
netcomsolution <33892389+netcomsolution@users.noreply.github.com>
2022-10-27 23:58:16 +0600

    f

--- whileloop.py
+++ whileloop.py
@@ -1,5 +1,33 @@
-# Method-1
-# n = 1
-# while n < 5:
-#     print("N value less than target")
-#     n += 1
+#Exercise 01
+# current_number = 0
+# while current_number <=10:
+#     #print(current_number)
+#     current_number +=1
+#     if current_number % 2 ==0:
+#         print(current_number)
+'''
+Exercise 01 result
+2
+4
+6
+8
+10
+'''
+#Exercise 02 Write a while loop that adds all the numbers up to 100
+# counter=0
+# total=0
+# while total <=100:
+#     #total += 1
+#     print(total)
+
+prompt = "\nTell me something, and i will repeat it back to you:"
+prompt += "\nEnter 'quit' to end program\n"
+
+active = True
+
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print("Please Write quit to end program")    
